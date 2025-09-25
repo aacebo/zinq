b76b0d1fc8cf1b2e2114dca275bca82e3b923730
aacebo <aacebowork@gmail.com>
2025-09-25 16:53:20 -0400

    Update lib.rs

--- zinq-reflect-macros/src/lib.rs
+++ zinq-reflect-macros/src/lib.rs
@@ -8,7 +8,7 @@ mod reflect_visibility;
 use proc_macro::TokenStream;
 
 #[proc_macro_derive(Reflect, attributes(reflect))]
-pub fn derive(tokens: TokenStream) -> TokenStream {
+pub fn derive_reflect(tokens: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(tokens as syn::DeriveInput);
 
     return match &input.data {
