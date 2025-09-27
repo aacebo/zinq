773873f0e67ace5feb84865190cfb417fa741d29
aacebo <aacebowork@gmail.com>
2025-09-27 14:04:02 -0400

    stuff

---
+++ zinq-reflect/src/primitive/mod.rs
@@ -0,0 +1,13 @@
+mod bool;
+pub mod number;
+mod ptr;
+pub mod slice;
+mod string;
+mod tuple;
+
+pub use bool::*;
+pub use number::*;
+pub use ptr::*;
+pub use slice::*;
+pub use string::*;
+pub use tuple::*;
