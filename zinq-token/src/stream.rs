f6379820a1dc91d1373475cae6f2f47cf9441e1c
aacebo <aacebowork@gmail.com>
2025-12-28 17:38:59 -0500

    Update stream.rs

--- zinq-token/src/stream.rs
+++ zinq-token/src/stream.rs
@@ -1,22 +1,16 @@
 use std::{ops::Index, str::FromStr};
 
 use zinq_error::ZinqError;
-use zinq_parse::{Parse, Parser, Span};
+use zinq_parse::{Parser, Span};
 
 use crate::{Token, TokenParser};
 
 #[derive(Debug, Default, Clone)]
-pub struct TokenStream {
-    span: Span,
-    tokens: Vec<Token>,
-}
+pub struct TokenStream(Vec<Token>);
 
 impl TokenStream {
     pub fn new() -> Self {
-        Self {
-            span: Span::from_str(""),
-            tokens: vec![],
-        }
+        Self(vec![])
     }
 }
 
@@ -34,58 +28,25 @@ impl FromStr for TokenStream {
             tokens.push(token);
         }
 
-        Ok(Self {
-            span: span.clone(),
-            tokens,
-        })
+        Ok(Self(tokens))
     }
 }
 
 impl From<Token> for TokenStream {
     fn from(value: Token) -> Self {
-        Self {
-            span: value.span().clone(),
-            tokens: vec![value],
-        }
+        Self(vec![value])
     }
 }
 
 impl FromIterator<Token> for TokenStream {
     fn from_iter<T: IntoIterator<Item = Token>>(iter: T) -> Self {
-        let tokens = iter.into_iter().collect::<Vec<Token>>();
-
-        if !tokens.is_empty() {
-            return Self {
-                span: Span::from_bounds(
-                    tokens.first().expect("must not be empty").span(),
-                    tokens.last().expect("must not be empty").span(),
-                ),
-                tokens,
-            };
-        }
-
-        Self::default()
+        Self(iter.into_iter().collect::<Vec<Token>>())
     }
 }
 
 impl FromIterator<Self> for TokenStream {
     fn from_iter<T: IntoIterator<Item = Self>>(iter: T) -> Self {
-        let tokens = iter
-            .into_iter()
-            .flat_map(|v| v.tokens)
-            .collect::<Vec<Token>>();
-
-        if !tokens.is_empty() {
-            return Self {
-                span: Span::from_bounds(
-                    tokens.first().expect("must not be empty").span(),
-                    tokens.last().expect("must not be empty").span(),
-                ),
-                tokens,
-            };
-        }
-
-        Self::default()
+        Self(iter.into_iter().flat_map(|v| v.0).collect::<Vec<Token>>())
     }
 }
 
@@ -94,33 +55,29 @@ impl IntoIterator for TokenStream {
     type IntoIter = std::vec::IntoIter<Token>;
 
     fn into_iter(self) -> Self::IntoIter {
-        self.tokens.into_iter()
+        self.0.into_iter()
     }
 }
 
 impl Extend<Token> for TokenStream {
     fn extend<T: IntoIterator<Item = Token>>(&mut self, iter: T) {
-        self.tokens.extend(iter);
-        self.span = Span::from_bounds(
-            self.tokens.first().expect("must not be empty").span(),
-            self.tokens.last().expect("must not be empty").span(),
-        );
+        self.0.extend(iter)
     }
 }
 
 impl Extend<Self> for TokenStream {
     fn extend<T: IntoIterator<Item = Self>>(&mut self, iter: T) {
-        self.tokens.extend(iter.into_iter().flat_map(|v| v.tokens));
-        self.span = Span::from_bounds(
-            self.tokens.first().expect("must not be empty").span(),
-            self.tokens.last().expect("must not be empty").span(),
-        );
+        self.0.extend(iter.into_iter().flat_map(|v| v.0))
     }
 }
 
 impl std::fmt::Display for TokenStream {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", &self.span)
+        for token in &self.0 {
+            write!(f, "{}", token)?;
+        }
+
+        Ok(())
     }
 }
 
@@ -128,7 +85,7 @@ impl std::ops::Deref for TokenStream {
     type Target = [Token];
 
     fn deref(&self) -> &Self::Target {
-        &self.tokens
+        &self.0
     }
 }
 
@@ -136,12 +93,12 @@ impl Eq for TokenStream {}
 
 impl PartialEq for TokenStream {
     fn eq(&self, other: &Self) -> bool {
-        if self.tokens.len() != other.tokens.len() {
+        if self.0.len() != other.0.len() {
             return false;
         }
 
-        for i in 0..self.tokens.len() {
-            if self.tokens.index(i) != other.tokens.index(i) {
+        for i in 0..self.0.len() {
+            if self.0.index(i) != other.0.index(i) {
                 return false;
             }
         }
@@ -150,29 +107,29 @@ impl PartialEq for TokenStream {
     }
 }
 
-// #[cfg(test)]
-// mod test {
-//     use std::str::FromStr;
+#[cfg(test)]
+mod test {
+    use std::str::FromStr;
 
-//     use zinq_error::Result;
+    use zinq_error::Result;
 
-//     use crate::TokenStream;
+    use crate::TokenStream;
 
-//     #[test]
-//     fn should_parse_str() -> Result<()> {
-//         let mut stream = TokenStream::from_str("let test: string = \"test\";")?;
+    #[test]
+    fn should_parse_str() -> Result<()> {
+        let mut stream = TokenStream::from_str("let test: string = \"test\";")?;
 
-//         debug_assert_eq!(stream.len(), 7);
-//         debug_assert_eq!(stream.to_string(), "let test: string = \"test\";");
+        debug_assert_eq!(stream.len(), 7);
+        debug_assert_eq!(stream.to_string(), "lettest:string=\"test\";");
 
-//         stream.extend(TokenStream::from_str("\nprintln(\"hello world\");")?);
+        stream.extend(TokenStream::from_str("\nprintln(\"hello world\");")?);
 
-//         debug_assert_eq!(stream.len(), 12);
-//         debug_assert_eq!(
-//             stream.to_string(),
-//             "let test: string = \"test\";\nprintln(\"hello world\");"
-//         );
+        debug_assert_eq!(stream.len(), 12);
+        debug_assert_eq!(
+            stream.to_string(),
+            "lettest:string=\"test\";println(\"hello world\");"
+        );
 
-//         Ok(())
-//     }
-// }
+        Ok(())
+    }
+}
